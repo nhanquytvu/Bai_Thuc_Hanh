fe87f0a3fe8a98ede5ba4ad2303d60bf44c15cf0
nhanquytvu <116791593+nhanquytvu@users.noreply.github.com>
2022-10-31 20:23:49 +0700

    Update BaiTH1.cpp

--- BaiTH1.cpp
+++ BaiTH1.cpp
@@ -11,25 +11,31 @@ int main()
 	 
 	 return 0;
 }
-
-void hello()
-{
-	printf("Hello World!");
-}
-
-// tinh giai thua ~ kieu tra ve long
-long tinhgiaithua(int n)
+// tinh tong 2 so nguyen duong
+int tong (int n)
     {
-    printf( "\n Nhap so n: ");
-    scanf("%d",n);
-       if (n==0)
-    return 1;
-        
-    long giaithua = 1;
-        for(int i=1; i<=n; i++)
-        giaithua=giaithua*i;
-    printf("\n Giai Thua cua mot so nguyen duong n la %d", giaithua);
-    return giaithua;
-	}
-
+	int a, b;
+	int tong=0;
+    printf("\n Nhap so nguyen duong a:");
+    scanf(" %d", &a);
+    printf("\n Nhap so nguyen duong b:");
+    scanf(" %d", &b);
+     tong=a+b;
+     printf("\n Tong 2 so nguyen duong a va b la %d + %d = %d", a, b, tong);
+return 0;
+     }
+ // tinh dien tich hinh chu nhat
+ int DienTichHCN (int n)
+    {
+	int a, b;
+	int DT=1;
+    printf("\n Nhap chieu dai a la:");
+    scanf(" %d", &a);
+    printf("\n Nhap chieu rong b la:");
+    scanf(" %d", &b);
+     DT=a*b;
+     printf("\n Dien Tich HCN co chieu dai a la %d va chieu rong b la %d = %d", a, b, DT);
+return 0;
+     }
+// tim USCLN cua 2 so nguyen a,b
 
