823fba1707293f2de1369718b557080864483e83
nhanquytvu <116791593+nhanquytvu@users.noreply.github.com>
2022-11-01 08:52:13 +0700

    Update BaiTH1.cpp

--- BaiTH1.cpp
+++ BaiTH1.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 void hello();
 long tinhgiaithua(int n);
+nt tong (int n);
+int DienTichHCN (int n);
 
 //  xay dung ham in Hello World ~ kieu void
 
@@ -8,10 +10,33 @@ int main()
 {
 	 hello();
 	 tinhgiaithua( 10 );
-	 
+	 tong(10);
+	 DienTichHCN(10);
 	 return 0;
 }
+void hello()
+{
+	printf("Hello World!");
+}
+
+// tinh giai thua ~ kieu tra ve long
+
+long tinhgiaithua(int n)
+    {
+    printf( "\n Nhap so n: ");
+    scanf("%d",&n);
+       if (n==0)
+    return 1;
+        
+    long giaithua = 1;
+        for(int i=1; i<=n; i++)
+        giaithua=giaithua*i;
+    printf("\n Giai Thua cua mot so nguyen duong n la %d", giaithua);
+ return giaithua;
+	}
+
 // tinh tong 2 so nguyen duong
+
 int tong (int n)
     {
 	int a, b;
